af1b70cb87b8dd41e4ba75e53f3ecc61872edcea
Christian Andreas Lærum-Onsager <christian.lerum-onsager@bouvet.no>
2020-11-08 18:32:10 +0100

    Extract style in Line component

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -10,21 +10,19 @@ type Props = {
 
 const Line: React.FC<Props> = ({ x1, y1, x2, y2 }) => {
   return (
-    <>
-      <svg
-        style={{
-          left: 0,
-          top: 0,
-          position: "absolute",
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        <StyledLine x1={x1} y1={y1} x2={x2} y2={y2} />
-      </svg>
-    </>
+    <StyledSvg>
+      <StyledLine x1={x1} y1={y1} x2={x2} y2={y2} />
+    </StyledSvg>
   );
 };
+
+const StyledSvg = styled.svg`
+  left: 0;
+  top: 0;
+  position: "absolute";
+  height: 100vh;
+  width: 100%;
+`;
 
 const StyledLine = styled.line`
   stroke: #071a39;
